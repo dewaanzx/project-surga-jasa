0b987fe507f913649e8e75cd5679ff4e27eba9bd
MariaTriElsa <mariatrielsa474@gmail.com>
2023-05-29 15:35:59 +0700

    configurasi database

--- backend-surga-jasa/application/config/config.php
+++ backend-surga-jasa/application/config/config.php
@@ -36,7 +36,7 @@ $config['base_url'] = 'http://localhost/project-surga-jasa/backend-surga-jasa';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
