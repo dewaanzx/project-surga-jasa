5753c94e050ff770bffc8faa18213c4e2246d8d3
Hilmarch27 <hilmanmahasiswa@gmail.com>
2023-06-19 12:07:09 +0700

    update hide button

--- backend-surga-jasa/application/views/auth/registration.php
+++ backend-surga-jasa/application/views/auth/registration.php
@@ -7,7 +7,7 @@
   <title>Registration - SJ</title>
   <link href="<?php echo base_url('application/assetsfe/bootstrap/css/bootstrap.min.css');?>" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
-    <link rel="shortcut icon" href="<?php echo base_url('../application\assetsfe\img\logo\faviconsj.ico');?>">
+    <link rel="shortcut icon" href="application/assetsfe/img/logo/faviconsj.ico">
 </head>
 
 <body>
